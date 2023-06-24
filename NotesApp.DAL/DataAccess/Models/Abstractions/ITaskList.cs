0af24430a62f80ca56d103792a62be13f395e725
Anton Kravchenko <kravchenko.s1111@gmail.com>
2023-06-24 20:30:47 +0300

    Added models and attribute

---
+++ NotesApp.DAL/DataAccess/Models/Abstractions/ITaskList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace NotesApp.DAL.DataAccess.Models.Abstractions
+{
+	public interface ITaskList : IDocument
+	{
+        public ObjectId OwnerId { get; set; }
+        public List<ObjectId> SharedWith { get; set; }
+    }
+}
+
