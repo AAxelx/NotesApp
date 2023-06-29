b2936273f7868b24945989f3088c284fcbc11cd6
Anton Kravchenko <kravchenko.s1111@gmail.com>
2023-06-29 17:03:12 +0300

    Added result models and enum

---
+++ NotesApp.Services/Models/ServiceResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NotesApp.Services.Models.Enums;
+
+namespace NotesApp.Services.Models
+{
+    public class ServiceResult
+    {
+        public ErrorType ErrorType { get; set; }
+
+        public ServiceResult(ErrorType type)
+        {
+            ErrorType = type;
+        }
+    }
+}
+
