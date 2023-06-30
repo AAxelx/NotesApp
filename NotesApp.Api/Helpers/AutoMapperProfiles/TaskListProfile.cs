d67c736c8e6329bc63e83a33959ebf38270b4bf1
Anton Kravchenko <kravchenko.s1111@gmail.com>
2023-06-30 23:33:10 +0300

    Added Automapper

---
+++ NotesApp.Api/Helpers/AutoMapperProfiles/TaskListProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using NotesApp.Api.Models.Requests.TaskList;
+using NotesApp.Api.Models.TaskList;
+using NotesApp.DAL.DataAccess.Models;
+
+namespace NotesApp.Api.Helpers.AutoMapperProfiles
+{
+	public class TaskListProfile : Profile
+	{
+		public TaskListProfile()
+		{
+            CreateMap<TaskListDto, TaskList>()
+				.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id.Value));
+
+            CreateMap<CreateTaskListDto, TaskListDto>();
+
+            CreateMap<UpdateTaskListDto, TaskListDto>();
+        }
+	}
+}
