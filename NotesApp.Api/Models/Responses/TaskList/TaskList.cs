5acd0d452c28b8a6808a036aba52b2e725b20d9c
Anton Kravchenko <kravchenko.s1111@gmail.com>
2023-06-30 23:32:46 +0300

    Added models

---
+++ NotesApp.Api/Models/Responses/TaskList/TaskList.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+
+namespace NotesApp.Api.Models.TaskList
+{
+	public class TaskList
+	{
+        public ObjectId Id { get; set; }
+
+        public string Title { get; set; }
+
+        public List<string> Content { get; set; }
+
+        public DateTime LastUpdatedAt { get; set; }
+
+        public ObjectId OwnerId { get; set; }
+
+        public List<ObjectId> SharedAccessUserIds { get; set; }
+    }
+}
