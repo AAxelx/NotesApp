26fa6957c77183b915f7fff30cf610b267067f39
Anton Kravchenko <78639975+AAxelx@users.noreply.github.com>
2023-07-17 22:03:09 +0300

    Renamed models and fixed bugs

--- NotesApp.Services/Services/TaskListService.cs
+++ NotesApp.Services/Services/TaskListService.cs
@@ -10,84 +10,84 @@ namespace NotesApp.Services.Services
 {
 	public class TaskListService : ITaskListService
     {
-        private readonly IMongoRepository<TaskListDto> _repository;
+        private readonly IMongoRepository<TaskList> _repository;
 
-		public TaskListService(IMongoRepository<TaskListDto> repository)
+		public TaskListService(IMongoRepository<TaskList> repository)
 		{
             _repository = repository;
 		}
 
-        public async Task<ServiceValueResult<TaskListDto>> GetByIdAsync(string listId, string userId)
+        public async Task<ServiceValueResult<TaskList>> GetByIdAsync(string listId, string userId)
         {
             var taskList = await _repository.GetByIdAsync(listId).ConfigureAwait(false);
 
             if (taskList == null)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.NotFound);
+                return new ServiceValueResult<TaskList>(ResponseType.NotFound);
             }
 
             var hasAccess = IsUserHasAccess(taskList, userId);
 
             if (!hasAccess)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.Forbidden);
+                return new ServiceValueResult<TaskList>(ResponseType.Forbidden);
             }
 
-            return new ServiceValueResult<TaskListDto>(taskList);
+            return new ServiceValueResult<TaskList>(taskList);
         }
 
-        public async Task<ServiceValueResult<List<TaskListDto>>> GetAll(string userId, int pageNumber, int pageSize)
+        public async Task<ServiceValueResult<List<TaskList>>> GetAll(string userId, int pageNumber, int pageSize)
         {
-            var filterDefinition = Builders<TaskListDto>.Filter.Eq(t => t.OwnerId, userId) |
-                Builders<TaskListDto>.Filter.AnyEq(t => t.SharedAccessUserIds, userId);
+            var filterDefinition = Builders<TaskList>.Filter.Eq(t => t.OwnerId, userId) |
+                Builders<TaskList>.Filter.AnyEq(t => t.SharedAccessUserIds, userId);
 
-            var sortDefinition = Builders<TaskListDto>.Sort.Descending(t => t.LastUpdatedAt);
+            var sortDefinition = Builders<TaskList>.Sort.Descending(t => t.LastUpdatedAt);
 
-            var projectionDefinition = Builders<TaskListDto>.Projection
+            var projectionDefinition = Builders<TaskList>.Projection
                 .Include(t => t.Id)
                 .Include(t => t.Title);
 
             var result = await _repository.GetAll(pageNumber, pageSize, filterDefinition, sortDefinition, projectionDefinition).ConfigureAwait(false);
 
-            return new ServiceValueResult<List<TaskListDto>>(result.ToList());
+            return new ServiceValueResult<List<TaskList>>(result.ToList());
         }
 
-        public async Task<ServiceValueResult<TaskListDto>> CreateAsync(TaskListDto taskList) //add user is created and owner check
+        public async Task<ServiceValueResult<TaskList>> CreateAsync(TaskList taskList) //add user is created and owner check
         {
             var createdTaskList = await _repository.CreateAsync(taskList).ConfigureAwait(false);
 
             if (createdTaskList.Id == null)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.BadRequest);
+                return new ServiceValueResult<TaskList>(ResponseType.BadRequest);
             }
 
-            return new ServiceValueResult<TaskListDto>(createdTaskList);
+            return new ServiceValueResult<TaskList>(createdTaskList);
         }
 
-        public async Task<ServiceValueResult<TaskListDto>> UpdateAsync(TaskListDto updateTaskList, string userId)
+        public async Task<ServiceValueResult<TaskList>> UpdateAsync(TaskList updateTaskList, string userId)
         {
             var taskList = await _repository.GetByIdAsync(updateTaskList.Id).ConfigureAwait(false);
 
             if(taskList == null)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.NotFound);
+                return new ServiceValueResult<TaskList>(ResponseType.NotFound);
             }
 
             var hasAccess = IsUserHasAccess(taskList, userId);
 
             if (!hasAccess)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.Forbidden);
+                return new ServiceValueResult<TaskList>(ResponseType.Forbidden);
             }
 
             var isSuccess = await _repository.UpdateOneAsync(updateTaskList).ConfigureAwait(false);
 
             if (!isSuccess)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.BadRequest);
+                return new ServiceValueResult<TaskList>(ResponseType.BadRequest);
             }
 
-            return new ServiceValueResult<TaskListDto>(updateTaskList);
+            return new ServiceValueResult<TaskList>(updateTaskList);
         }
         
         public async Task<ServiceResult> DeleteAsync(string listId, string userId)
@@ -114,31 +114,35 @@ namespace NotesApp.Services.Services
             return new ServiceResult(ResponseType.NoContent);
         }
 
-        public async Task<ServiceValueResult<TaskListDto>> AddUserAccessAsync(string listId, string userId, string newUserAccessId)
+        public async Task<ServiceValueResult<TaskList>> AddUserAccessAsync(string listId, string userId, string newUserAccessId)
         {
             var taskList = await _repository.GetByIdAsync(listId).ConfigureAwait(false);
 
             if(taskList == null)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.NotFound);
+                return new ServiceValueResult<TaskList>(ResponseType.NotFound);
             }
 
             var hasAccess = IsUserHasAccess(taskList, userId);
 
             if (!hasAccess)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.Forbidden);
+                return new ServiceValueResult<TaskList>(ResponseType.Forbidden);
             }
 
+            if (taskList.SharedAccessUserIds.Contains(newUserAccessId))
+            {
+                return new ServiceValueResult<TaskList>(ResponseType.Ok);
+            }
             taskList.SharedAccessUserIds.Add(newUserAccessId);
             var isSuccess = await _repository.UpdateOneAsync(taskList).ConfigureAwait(false);
 
             if (!isSuccess)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.BadRequest);
+                return new ServiceValueResult<TaskList>(ResponseType.BadRequest);
             }
 
-            return new ServiceValueResult<TaskListDto>(taskList);
+            return new ServiceValueResult<TaskList>(taskList);
         }
 
         public async Task<ServiceValueResult<List<string>>> GetUserAccessListAsync(string listId, string userId)
@@ -160,20 +164,20 @@ namespace NotesApp.Services.Services
             return new ServiceValueResult<List<string>>(taskList.SharedAccessUserIds);
         }
 
-        public async Task<ServiceValueResult<TaskListDto>> RemoveUserAccessAsync(string listId, string userId, string userIdForRemove)
+        public async Task<ServiceValueResult<TaskList>> RemoveUserAccessAsync(string listId, string userId, string userIdForRemove)
         {
             var taskList = await _repository.GetByIdAsync(listId).ConfigureAwait(false);
 
             if (taskList == null)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.NotFound);
+                return new ServiceValueResult<TaskList>(ResponseType.NotFound);
             }
 
             var hasAccess = IsUserHasAccess(taskList, userId);
 
             if (!hasAccess)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.Forbidden);
+                return new ServiceValueResult<TaskList>(ResponseType.Forbidden);
             }
 
             taskList.SharedAccessUserIds.Remove(userIdForRemove);
@@ -181,13 +185,13 @@ namespace NotesApp.Services.Services
 
             if (!isSuccess)
             {
-                return new ServiceValueResult<TaskListDto>(ResponseType.BadRequest);
+                return new ServiceValueResult<TaskList>(ResponseType.BadRequest);
             }
 
-            return new ServiceValueResult<TaskListDto>(taskList);
+            return new ServiceValueResult<TaskList>(taskList);
         }
 
-        private bool IsUserHasAccess(TaskListDto taskList, string userId)
+        private bool IsUserHasAccess(TaskList taskList, string userId)
         {
             return taskList.OwnerId == userId || taskList.SharedAccessUserIds.Contains(userId);
         }
